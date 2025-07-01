52b37c22a2f0c5bcb9f84d412fe12b8019098421
Walid Chtioui <walid.chtioui@ensi-uma.tn>
2025-07-01 07:51:24 +0200

    improve metadata UI;

--- Runtime/Scripts/RuntimeUI/DatasetMetadataUI.cs
+++ Runtime/Scripts/RuntimeUI/DatasetMetadataUI.cs
@@ -8,32 +8,27 @@ namespace UnityCTVisualizer
         //////////////////////////////
         /// TMP InputFields
         //////////////////////////////
-        [SerializeField] TMP_InputField m_OriginalVolumeWidth;
-        [SerializeField] TMP_InputField m_OriginalVolumeHeight;
-        [SerializeField] TMP_InputField m_OriginalVolumeDepth;
-        [SerializeField] TMP_InputField m_VDHMeasure;
+        [SerializeField] TMP_InputField m_OriginalVolumeDimensions;
         [SerializeField] TMP_InputField m_ChunkSize;
         [SerializeField] TMP_InputField m_ColorDepth;
-        [SerializeField] TMP_InputField m_VolumetricObjectWidth;
-        [SerializeField] TMP_InputField m_VolumetricObjectHeight;
-        [SerializeField] TMP_InputField m_VolumetricObjectDepth;
         [SerializeField] TMP_InputField m_LZ4Compressed;
         [SerializeField] TMP_InputField m_DecompressedSize;
+        [SerializeField] TMP_InputField m_InterBrickInterpolation;
+        [SerializeField] TMP_InputField m_NbrResolutionLvls;
+        [SerializeField] TMP_InputField m_OctreeSmallestSubdivision;
+        [SerializeField] TMP_InputField m_ConvertedTo8bpp;
 
 
         void Awake()
         {
-            m_OriginalVolumeWidth.readOnly = true;
-            m_OriginalVolumeHeight.readOnly = true;
-            m_OriginalVolumeDepth.readOnly = true;
-            m_VDHMeasure.readOnly = true;
+            m_OriginalVolumeDimensions.readOnly = true;
             m_ChunkSize.readOnly = true;
             m_ColorDepth.readOnly = true;
-            m_VolumetricObjectWidth.readOnly = true;
-            m_VolumetricObjectHeight.readOnly = true;
-            m_VolumetricObjectDepth.readOnly = true;
             m_LZ4Compressed.readOnly = true;
             m_DecompressedSize.readOnly = true;
+            m_InterBrickInterpolation.readOnly = true;
+            m_NbrResolutionLvls.readOnly = true;
+            m_OctreeSmallestSubdivision.readOnly = true;
         }
 
         /// <summary>
@@ -46,17 +41,15 @@ namespace UnityCTVisualizer
         /// </param>
         public void Init(CVDSMetadata metadata)
         {
-            m_OriginalVolumeWidth.text = metadata.Dims.x.ToString();
-            m_OriginalVolumeHeight.text = metadata.Dims.y.ToString();
-            m_OriginalVolumeDepth.text = metadata.Dims.z.ToString();
-            m_VDHMeasure.text = "-";
+            m_OriginalVolumeDimensions.text = $"{metadata.Dims.x}x{ metadata.Dims.y}x{metadata.Dims.z}";
             m_ChunkSize.text = metadata.ChunkSize.ToString();
             m_ColorDepth.text = $"{metadata.ColorDepth}";
-            m_VolumetricObjectWidth.text = (metadata.VoxelDims.x * metadata.Dims.x).ToString("0.00");
-            m_VolumetricObjectHeight.text = (metadata.VoxelDims.y * metadata.Dims.x).ToString("0.00");
-            m_VolumetricObjectDepth.text = (metadata.VoxelDims.z * metadata.Dims.x).ToString("0.00");
             m_LZ4Compressed.text = metadata.Lz4Compressed ? "true" : "false";
             m_DecompressedSize.text = ((metadata.Dims.x * metadata.Dims.y * metadata.Dims.z) / (1024.0f * 1024.0f)).ToString("0.00");
+            m_InterBrickInterpolation.text = metadata.ChunkPadding ? "true" : "false";
+            m_NbrResolutionLvls.text = metadata.NbrResolutionLvls.ToString();
+            m_OctreeSmallestSubdivision.text = metadata.OctreeSmallestSubdivision.ToString();
+            m_ConvertedTo8bpp.text = metadata.ConvertedToUInt8 ? "true" : "false";
         }
     }
 }
